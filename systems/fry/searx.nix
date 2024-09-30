8c5cd9998744b9a2e83b15c8bb92377fae403e9e
Xaver Himmelsbach <xaver.himmelsbach@gmail.com>
2024-09-30 23:20:21 +0200

    Setup sub-domain for searx

--- systems/fry/searx.nix
+++ systems/fry/searx.nix
@@ -4,6 +4,34 @@
     file = ../../secrets/searx-environment.age;
   };
 
+  users.groups."acme-search.himmelsbach.dev" = {
+    members = [
+      "acme"
+      "nginx"
+    ];
+  };
+
+  security.acme = {
+    certs."search.himmelsbach.dev" = {
+      dnsProvider = "hetzner";
+      environmentFile = config.age.secrets.hetzner-api-key.path;
+      group = "acme-search.himmelsbach.dev";
+    };
+  };
+
+  services.nginx = {
+    # TODO: SLD as variable?
+    virtualHosts."search.himmelsbach.dev" =  {
+      forceSSL = true;
+      sslCertificate = "/var/lib/acme/search.himmelsbach.dev/cert.pem";
+      sslCertificateKey = "/var/lib/acme/search.himmelsbach.dev/key.pem";
+      locations."/" = {
+        # TODO: Port variable
+        proxyPass = "http://127.0.0.1:8888";
+      };
+    };
+  };
+
   services.searx = {
     enable = true;
     environmentFile = config.age.secrets.searx-environment.path;
