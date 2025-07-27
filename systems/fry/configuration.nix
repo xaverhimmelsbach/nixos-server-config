f4589923b6be87b2883447125f0cc2d371524c41
Xaver Himmelsbach <xaver.himmelsbach@gmail.com>
2025-07-27 18:26:04 +0200

    Add id_hetzner_sftp key

--- systems/fry/configuration.nix
+++ systems/fry/configuration.nix
@@ -23,8 +23,9 @@
     openFirewall = false;
   };
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOU6AXkMMuvqnuF+5hEqYRHvbAebES8XUo2eVfxRjcD"
-  ]; # id_hetzner
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOU6AXkMMuvqnuF+5hEqYRHvbAebES8XUo2eVfxRjcD xaver@standin" # id_hetzner
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtcmtJaH+j3jb3DOnvHTEDBv1hTeWtLEbwPARgSVgbX xaver@standin" # id_hetzner_sftp
+  ];
 
   system.stateVersion = stateVersion;
 }
